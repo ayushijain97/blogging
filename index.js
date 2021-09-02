2816409d12e1787a75ad02a182e814eaa919812c
ayushijain97 <neeraj_jain@apple.com>
2021-09-02 09:50:38 +0530

    post blog and store it to database

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
- setTimeout(function a(){
-     console.log("Ayushi");
- },5000)
- a();
+const createBlog = async(e) => {
+    let inputData = document.querySelector(".form_inputField").value;
+    // const today = new Date();
+    // let month = today.toLocaleString("default", { month: "short" });
+    // if (today.getFullYear)
+    //   console.log(today.getDate() + " " + month + " " + today.getFullYear());
+    
+    e.preventDefault();
+    try {
+        const response = await fetch(
+            "http://localhost:3000/blog",
+             {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/JSON",
+                },
+                body: JSON.stringify({
+                     inputData: inputData,
+                }),
+            }
+        );
+        const data = await response.text();
+        // enter you logic when the fetch is successful
+        console.log(data);
+      } catch (error) {
+        // enter your logic for when there is an error (ex. error toast)
+        console.log(error);
+      }
+ };
+document.querySelector(".formBtn").addEventListener("click",createBlog);
+
+
